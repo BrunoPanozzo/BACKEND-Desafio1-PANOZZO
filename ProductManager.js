af3effb0c8eab2ba9972a10b87015735a3d1103e
BrunoPanozzo <brunopanozzo@hotmail.com>
2024-02-09 20:54:21 -0300

    versión final del primer desafio

--- ProductManager.js
+++ ProductManager.js
@@ -15,41 +15,69 @@ class ProductManager {
         return ProductManager.#lastID_Product
     }
 
-    //metodos públicos
+    //validar un string permitiendo solo números y letras
+    #soloLetrasYNumeros = (cadena) => {
+        return (/^[a-z A-Z 0-9]+$/.test(cadena))
+    }
+
+    //métodos públicos
+
+    //devuelve todo el arreglo de productos
     getProducts = () => {
         return this.#products
     }    
 
-    //función para validar un string permitiendo solo números y letras
-    soloLetrasYNumeros = (cadena) => {
-        return (/^[a-z A-Z 0-9]+$/.test(cadena))
+    //dado un ID busca en el arreglo de productos un producto con dicho ID, caso contrario devuelve msje de error
+    getProductById = (code) => {
+        const producto = this.#products.find(item => item.id === code)
+        if (producto)
+            return producto
+        else {
+            console.error(`El producto con código ${code} no existe`)
+            return
+        }
     }
 
+    
+
+    //permite agregar un producto al arreglo de productos inicial si cumple con ciertas validaciones
     addProduct = (title, description, price, thumbnail, code, stock) => {
-                
-        if (title.trim().length > 0) {
+        //validar que el campo "title" no esté vacío        
+        if (title.trim().length <= 0) {
             console.error("El campo \"title\" es inválido")
             return
         }
-        if (description.trim().length > 0) {
+        //validar que el campo "description" no esté vacío
+        if (description.trim().length <= 0) {
             console.error("El campo \"description\" es inválido")
             return
         }
+        //validar que el campo "price" contenga sólo números
         if (isNaN(price)) {
             console.error("El campo \"price\" no es un número")
             return
         }
-        if (thumbnail.trim().length > 0) {
+        //validar que el campo "thumbnail" no esté vacío
+        if (thumbnail.trim().length <= 0) {
             console.error("El campo \"thumbnail\" es inválido")
             return
         }
+        //validar que el campo "code" contenga sólo números y letras
         const codeAValidar = code.trim()
-        if (codeAValidar.length > 0 && soloLetrasYNumeros(codeAValidar)) {
+        if ((codeAValidar.length <= 0) || (!this.#soloLetrasYNumeros(codeAValidar))) {
             console.error("El campo \"code\" es inválido")
             return
         }
+        //validar que el campo "stock" contenga sólo números
         if (isNaN(stock)) {
             console.error("El campo \"stock\" no es un número")
+            return
+        }        
+
+        //antes de agregar el producto, verificar que el campo "code" no se repita
+        const producto = this.#products.find(item => item.code === code)
+        if (producto) {
+            console.error(`El producto con código ${code} ya existe`)
             return
         }
 
@@ -68,3 +96,22 @@ class ProductManager {
     }
 
 }
+
+//testing de la clase
+const productManager = new ProductManager()
+console.log(productManager.getProducts())
+productManager.addProduct("producto prueba",
+                          "Este es un producto prueba",
+                          200,
+                          "sin imagen",
+                          "abc123",
+                          25)
+console.log(productManager.getProducts())
+productManager.addProduct("producto prueba",
+                          "Este es un producto prueba",
+                          200,
+                          "sin imagen",
+                          "abc123",
+                          25)
+console.log(productManager.getProductById(1))
+console.log(productManager.getProductById(2))
