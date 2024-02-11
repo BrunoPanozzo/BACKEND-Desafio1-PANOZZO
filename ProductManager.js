633eab1980959f18b355ab015e120b83a42e02ea
BrunoPanozzo <brunopanozzo@hotmail.com>
2024-02-11 13:28:28 -0300

    ajustes en las validaciones

--- ProductManager.js
+++ ProductManager.js
@@ -9,7 +9,8 @@ class ProductManager {
         this.#products = []
     }
 
-    //método interno
+    //métodos internos
+    //retornar un ID único para cada producto nuevo
     #getNuevoID = () => {
         ProductManager.#lastID_Product += 1
         return ProductManager.#lastID_Product
@@ -17,28 +18,30 @@ class ProductManager {
 
     //validar un string permitiendo solo números y letras
     #soloLetrasYNumeros = (cadena) => {
-        return (/^[a-z A-Z 0-9]+$/.test(cadena))
+        return (/^[a-zA-Z0-9]+$/.test(cadena))
+    }
+
+    //validar permitiendo solo números
+    #soloNumeros = (cadena) => {
+        return (/^[0-9]+$/.test(cadena))
     }
 
     //métodos públicos
-
-    //devuelve todo el arreglo de productos
+    //devolver todo el arreglo de productos
     getProducts = () => {
         return this.#products
     }    
 
-    //dado un ID busca en el arreglo de productos un producto con dicho ID, caso contrario devuelve msje de error
+    //dado un ID buscar en el arreglo de productos un producto con dicho ID, caso contrario devolver msje de error
     getProductById = (code) => {
         const producto = this.#products.find(item => item.id === code)
         if (producto)
             return producto
         else {
-            console.error(`El producto con código ${code} no existe`)
+            console.error(`El producto con código \"${code}\" no existe`)
             return
         }
-    }
-
-    
+    }    
 
     //permite agregar un producto al arreglo de productos inicial si cumple con ciertas validaciones
     addProduct = (title, description, price, thumbnail, code, stock) => {
@@ -53,7 +56,7 @@ class ProductManager {
             return
         }
         //validar que el campo "price" contenga sólo números
-        if (isNaN(price)) {
+        if ((!this.#soloNumeros(price)) || (typeof price != "number")) {
             console.error("El campo \"price\" no es un número")
             return
         }
@@ -69,7 +72,7 @@ class ProductManager {
             return
         }
         //validar que el campo "stock" contenga sólo números
-        if (isNaN(stock)) {
+        if ((!this.#soloNumeros(stock)) || (typeof stock != "number")) {
             console.error("El campo \"stock\" no es un número")
             return
         }        
@@ -77,7 +80,7 @@ class ProductManager {
         //antes de agregar el producto, verificar que el campo "code" no se repita
         const producto = this.#products.find(item => item.code === code)
         if (producto) {
-            console.error(`El producto con código ${code} ya existe`)
+            console.error(`No se permite agregar el producto con código \"${code}\" porque ya existe`)
             return
         }
 
@@ -86,10 +89,10 @@ class ProductManager {
             id : this.#getNuevoID(),
             title,
             description,
-            price,
+            price: Number(price),
             thumbnail,
             code,
-            stock
+            stock: Number(stock)
         }
 
         this.#products.push(product)
@@ -97,21 +100,34 @@ class ProductManager {
 
 }
 
-//testing de la clase
+//testing de la clase "ProductManager"
 const productManager = new ProductManager()
-console.log(productManager.getProducts())
+
+let products = productManager.getProducts()
+console.log(products)
+
 productManager.addProduct("producto prueba",
                           "Este es un producto prueba",
                           200,
                           "sin imagen",
                           "abc123",
                           25)
-console.log(productManager.getProducts())
+
+products = productManager.getProducts()
+console.log(products)
+
 productManager.addProduct("producto prueba",
                           "Este es un producto prueba",
                           200,
                           "sin imagen",
                           "abc123",
                           25)
-console.log(productManager.getProductById(1))
-console.log(productManager.getProductById(2))
+
+products = productManager.getProducts()
+console.log(products)
+
+let product = productManager.getProductById(1)
+if (product) console.log(product)
+
+product = productManager.getProductById(2)
+if (product) console.log(product)
